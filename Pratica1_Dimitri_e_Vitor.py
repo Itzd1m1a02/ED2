42afd3b2674919e16e5476b8cd50dfc6114eb7f8
Dimitri pereira maia <93716158+Itzd1m1a02@users.noreply.github.com>
2023-04-02 23:53:44 -0300

    ATIVIDADE 1 ED2

--- Pratica1_Dimitri_e_Vitor.py
+++ Pratica1_Dimitri_e_Vitor.py
@@ -70,57 +70,47 @@ def insertionSort (vetor, opcao, cont):
         cont[0] += 1
     return vetor
 
-def MergeSort(op, V, inicio, fim, cont):
+def MergeSort(op, V, inicio, fim):
     if fim <= inicio:
-        cont[0] += 1
         return V
     else:
         meio = (inicio + fim) // 2
-        MergeSort(op, V, inicio, meio, cont)#caminha pela parte esquerda do vetor
-        MergeSort(op, V, meio+1, fim, cont)#caminha pela parte direita do vetor
-        Merge(op, V, inicio, meio, fim, cont)#função que verifica se o vetor unitario formado e mior ou menor e junta dois vetores  
-def Merge(op, V, inicio, meio, fim, cont):
+        MergeSort(op, V, inicio, meio)#caminha pela parte esquerda do vetor
+        MergeSort(op, V, meio+1, fim)#caminha pela parte direita do vetor
+        Merge(op, V, inicio, meio, fim)#função que verifica se o vetor unitario formado e mior ou menor e junta dois vetores 
+        
+def Merge(op, V, inicio, meio, fim):
     vaux = V[:] #copia o vetor principal para um auxiliar
     p1 = inicio #função que anda o vetor esquerdo(inicial) sem alterar o vetor original
     p2 = meio + 1#função que recebe o meior do vetor V para fraguimentação ate ter um ou mais vetores unitarios
     if (op == 'c'): # ordem crecente
-        cont[0] += 1
         for k in range(inicio, fim + 1): #for que vai ate o fim do vetor
             if p1 > meio: # se o ponto 1 da posição do inicio e maior que o numero no meio do vetor
                 V[k] = vaux[p2] #então o valor salvo naquela posição do vetor aux e colocado no veor original
                 p2 += 1 #posição + 1
-                cont[0] += 1
             elif p2 > fim: #se o ponto 2 da posição do inicio e maior que o fim
                 V[k] = vaux[p1]#então o valor salvo naquela posição do vetor aux e colocado no veor original
                 p1 += 1
-                cont[0] += 1
             elif vaux[p2] < vaux[p1]:#se o valor salvo no ponto p2 e menor que o valor salvo no p1
                 V[k] = vaux[p2] 
                 p2 += 1
-                cont[0] += 1
             else:
                 V[k] = vaux[p1]#se o valor salvo no ponto p1 e menor que o valor salvo no p2
                 p1 += 1
-                cont[0] += 1
     if (op == 'd'):#ordem decrecente
-        cont[0] += 1
         for k in range(inicio, fim + 1):
             if p1 > meio:
                 V[k] = vaux[p2]
                 p2 += 1
-                cont[0] += 1
             elif p2 > fim:
                 V[k] = vaux[p1]
                 p1 += 1
-                cont[0] += 1
             elif vaux[p2] > vaux[p1]:
                 V[k] = vaux[p2]
                 p2 += 1
-                cont[0] += 1
             else:
                 V[k] = vaux[p1]
                 p1 += 1
-                cont[0] += 1
  
 def Quicksort(V, inicio, fim, op):
     if inicio < fim:
@@ -167,23 +157,32 @@ while (aux < 0 or aux == 0):
 print('\n')
 #criação de um array como o tamanho lido do arquivo.
 array = random.randint(0,aux,size) #gera numeros aleatorios de acordo com o tamanho
-fim = len(array) - 1
+fim = size - 1
 inicio = 0
 j = [0]
+auxV = array.copy()
 
 #chamada de estruturas de ordenação
 print('==================================================================')
-BubbleSort(array, modo, j)
-print(f'\n\nVetor ordenado com Bubblesort:{array}, comp:{j}')
-selectsort(array, size, modo, j)
-print(f'\n\nVetor ordenado com Selectsort:{array}, comp:{j}')
-insertionSort(array, modo, j)
-print(f'\n\nVetor ordenado com Insertionsort:{array}, comp:{j}')
-MergeSort(modo, array, inicio, fim, j)
-print(f'\n\nVetor ordenado com Mergesort:{array}, comp:{j}')
+BubbleSort(auxV, modo, j)
+print(f'\n\nVetor ordenado com Bubblesort:{auxV}')
+auxV = array.copy()
 
-Quicksort(array, inicio, fim, modo)
-print(f'\n\nVetor ordenado com Quicksort:{array}')
+selectsort(auxV, size, modo, j)
+print(f'\n\nVetor ordenado com Selectsort:{auxV}')
+auxV = array.copy()
+
+insertionSort(auxV, modo, j)
+print(f'\n\nVetor ordenado com Insertionsort:{auxV}')
+
+
+MergeSort(modo, array, inicio, fim)
+print(f'\n\nVetor ordenado com Mergesort:{array}')
+auxV = array.copy()
+
+Quicksort(auxV, inicio, fim, modo)
+print(f'\n\nVetor ordenado com Quicksort:{auxV}')
 print('\n\n==================================================================\n')
+
 #finaliza o arq
 fp.close()
