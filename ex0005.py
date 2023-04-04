2de01258147d020f427c3edda4328b1f05cf5905
Dimitri pereira maia <93716158+Itzd1m1a02@users.noreply.github.com>
2023-04-04 13:32:15 -0300

    QuickSort

--- ex0005.py
+++ ex0005.py
@@ -1,24 +1,40 @@
-def Quicksort(V, inicio, fim):
-    if inicio < fim:
-        pivo = particionar(V, inicio, fim)
-        Quicksort(V, inicio, pivo-1)
-        Quicksort(V, pivo+1, fim)
-def particionar(V, inicio, fim):
-    esq = inicio
-    dir = fim
-    pivo = V[inicio]
-    while (esq < dir):
-        while (V[esq] <= pivo and esq <= fim):
-            esq += 1
-        while (V[dir] > pivo and dir >= inicio):
-            dir -= 1
-        if (esq < dir):
-            V[esq], V[dir] = V[dir], V[esq]
-    V[dir], V[inicio] = V[inicio], V[dir]
-    return dir
-   
-V = [-3,-16,18,36,-12,-25,-31,-15,13,16,0,-45,-41,-22,12]
-inicio = 0
-fim = len(V) - 1
-Quicksort(V, inicio, fim)
-print('Vetor ordenado: ', V)
+def Quicksort(array, inicio, fim, op, cont):
+   if inicio < fim:
+       pivo = particiona(array, inicio, fim, op, cont)
+       Quicksort(array,inicio,pivo-1, op, cont)
+       Quicksort(array,pivo+1,fim, op, cont)
+def particiona(array, inicio, fim, op, cont):
+    pivo = array[inicio]
+    esquerda = inicio+1
+    direita = fim
+    aux = False
+    
+    if op == 'c':
+        while not aux:
+            while esquerda <= direita and array[esquerda] <= pivo:
+                esquerda += 1
+            while array[direita] >= pivo and direita >= esquerda:
+                direita -= 1
+            if direita < esquerda:
+                aux = True
+            else:
+                array[esquerda], array[direita] = array[direita], array[esquerda]
+                cont[0] += 1
+        array[inicio], array[direita] = array[direita], array[inicio]
+        cont[0] += 1
+        return direita
+    
+    if op == 'd':
+        while not aux:
+            while esquerda <= direita and array[esquerda] >= pivo:
+                esquerda += 1
+            while array[direita] <= pivo and direita >= esquerda:
+                direita -= 1
+            if direita < esquerda:
+                aux = True
+            else:
+                array[esquerda], array[direita] = array[direita], array[esquerda]
+                cont[0] += 1
+        array[inicio], array[direita] = array[direita], array[inicio]
+        cont[0] += 1
+        return direita
