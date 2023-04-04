dae3dd1db122f51371626e885dcaaae2eaf498b8
Dimitri pereira maia <93716158+Itzd1m1a02@users.noreply.github.com>
2023-04-04 13:32:35 -0300

    MergeSort

--- ex0004.py
+++ ex0004.py
@@ -1,17 +1,16 @@
-def MergeSort(op, V, inicio, fim):
+def MergeSort(V, inicio, fim, op, cont):
     if fim <= inicio:
         return V
     else:
         meio = (inicio + fim) // 2
-        MergeSort(op, V, inicio, meio)#caminha pela parte esquerda do vetor
-        MergeSort(op, V, meio+1, fim)#caminha pela parte direita do vetor
-        Merge(op, V, inicio, meio, fim)#função que verifica se o vetor unitario formado e mior ou menor e junta dois vetores
-    
-def Merge(op, V, inicio, meio, fim):
+        MergeSort(V, inicio, meio, op, cont)#caminha pela parte esquerda do vetor
+        MergeSort(V, meio+1, fim, op, cont)#caminha pela parte direita do vetor
+        Merge(V, inicio, meio, fim, op, cont)#função que verifica se o vetor unitario formado e mior ou menor e junta dois vetores
+def Merge(V, inicio, meio, fim, op, cont):
     vaux = V[:] #copia o vetor principal para um auxiliar
     p1 = inicio #função que anda o vetor esquerdo(inicial) sem alterar o vetor original
     p2 = meio + 1#função que recebe o meior do vetor V para fraguimentação ate ter um ou mais vetores unitarios
-    if (op == 1): # ordem crecente
+    if (op == 'c'): # ordem crecente
         for k in range(inicio, fim + 1): #for que vai ate o fim do vetor
             if p1 > meio: # se o ponto 1 da posição do inicio e maior que o numero no meio do vetor
                 V[k] = vaux[p2] #então o valor salvo naquela posição do vetor aux e colocado no veor original
@@ -25,7 +24,8 @@ def Merge(op, V, inicio, meio, fim):
             else:
                 V[k] = vaux[p1]#se o valor salvo no ponto p1 e menor que o valor salvo no p2
                 p1 += 1
-    if (op == -1):#ordem decrecente
+                cont[0] += 1
+    if (op == 'd'):#ordem decrecente
         for k in range(inicio, fim + 1):
             if p1 > meio:
                 V[k] = vaux[p2]
@@ -39,12 +39,4 @@ def Merge(op, V, inicio, meio, fim):
             else:
                 V[k] = vaux[p1]
                 p1 += 1
-
-array = [-3, -16, 18, 36, -12, -25, -31, -15, 13, 16,  0, -45, -41, -22, 12] #vetor v
-inicio = 0 #inicio vale 0
-fim = len(array) - 1 #pega o tamanho final do vetor 
-op = int(input('Digite 1 para crescente e -1 para decrescente: '))
-while op == 0 or op < -1 or op > 1:
-    op = int(input('\nDigite 1 para crescente e -1 para decrescente: '))
-MergeSort(op, array, inicio, fim)
-print(f'Vetor ordenado:', array)
+                cont[0] += 1
